9fe5a8e3a22c91537885c0412836ecbabe8532b7
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-03-30 15:56:35 -0400

    Finalizing ajax demo

--- src/main/resources/static/assets/pages/scripts/profile.js
+++ src/main/resources/static/assets/pages/scripts/profile.js
@@ -1,4 +1,11 @@
 function employeeSelect() {
+	//display the spinner
+	$('#ajaxLoader').show();
+	
+	//first, let's get rid of the default "SELECT" option if it exists
+	var defaultOption = $("#employeeIdSelect option[value='default']");
+	if (defaultOption) defaultOption.remove();
+	
 	//get the selected id
 	var employeeId = $('#employeeIdSelect').val();
 	
@@ -6,29 +13,42 @@ function employeeSelect() {
 	var url = "./employee/" + employeeId;
 	
 	//do the ajax call
-	$.get(url, populateEmployeeInfo);
-	
-	$('#profileRow').show();
+	$.get(url, populateEmployeeInfo);	
 }
 
 //This is the function we run when we get back a response
 //from the ajax call
 function populateEmployeeInfo(data) {
-	alert("here");
-	
 	var status = data.responseStatus;
 	
-	//check the response to make sure it's ok;
+	//check the response to make sure it's ok
 	if (status == "Ok") {
 		var response = data.response;
 		
+		//get the JSON data
 		var department = response.department;
 		var employeeId = response.employeeId;
 		var firstName = response.firstName;
 		var lastName = response.lastName;
 		var hoursPerWeek = response.hoursPerWeek;
-		var imageFile = response.imageFile;
+		var imageFile = './images/' + response.imageFile;
 		
-		alert(department);
+		//set the profile badge values
+		$('#employeePic').attr("src", imageFile);
+		$('#employeeName').html(firstName + ' ' + lastName);
+		$('#employeeDepartment').html(department);
+		
+		//set the input field values
+		$('#employeeFirstName').val(firstName);
+		$('#employeeLastName').val(lastName);
+		$('#employeeDept').val(department);
+		$('#employeeId').val(employeeId);
+		$('#employeeHours').val(hoursPerWeek);
+		
+		//show the hidden elements
+		$('#profileRow').show();
 	}
+	
+	//hide the spinner again
+	$('#ajaxLoader').hide();
 }
